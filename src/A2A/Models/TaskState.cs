dd71a688668b7b9bc98e0e5c150a5cd7b0388436
Roman Konecny <rokonecn@microsoft.com>
2026-02-13 17:39:52 +0100

    feat: add ProtoJsonEnumConverter, update TaskState and MessageRole to v1.0 SCREAMING_SNAKE_CASE
    - Add ProtoJsonEnumConverter<T> using EnumMemberAttribute for proto-aligned serialization
    - TaskState: add Unspecified value, TASK_STATE_* serialized names
    - MessageRole: add Unspecified value, ROLE_USER/ROLE_AGENT serialized names
    - Existing tests expected to fail (use v0.3 JSON payloads)

--- src/A2A/Models/TaskState.cs
+++ src/A2A/Models/TaskState.cs
@@ -1,55 +1,71 @@
+using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace A2A;
 
 /// <summary>
-/// Represents the possible states of a Task.
+/// Defines the possible lifecycle states of a Task.
 /// </summary>
-[JsonConverter(typeof(KebabCaseLowerJsonStringEnumConverter<TaskState>))]
+[JsonConverter(typeof(ProtoJsonEnumConverter<TaskState>))]
 public enum TaskState
 {
     /// <summary>
+    /// The task is in an unknown or indeterminate state.
+    /// </summary>
+    [EnumMember(Value = "TASK_STATE_UNSPECIFIED")]
+    Unspecified,
+
+    /// <summary>
     /// Indicates that the task has been submitted.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_SUBMITTED")]
     Submitted,
 
     /// <summary>
     /// Indicates that the task is currently being worked on.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_WORKING")]
     Working,
 
     /// <summary>
     /// Indicates that the task requires input from the user.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_INPUT_REQUIRED")]
     InputRequired,
 
     /// <summary>
     /// Indicates that the task has been completed successfully.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_COMPLETED")]
     Completed,
 
     /// <summary>
     /// Indicates that the task has been canceled.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_CANCELED")]
     Canceled,
 
     /// <summary>
     /// Indicates that the task has failed.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_FAILED")]
     Failed,
 
     /// <summary>
     /// Indicates that the task has been rejected.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_REJECTED")]
     Rejected,
 
     /// <summary>
     /// Indicates that the task requires authentication.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_AUTH_REQUIRED")]
     AuthRequired,
 
     /// <summary>
     /// Indicates that the task state is unknown.
     /// </summary>
+    [EnumMember(Value = "TASK_STATE_UNKNOWN")]
     Unknown
 }
